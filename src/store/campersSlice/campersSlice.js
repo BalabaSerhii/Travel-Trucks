b1a2dd7a234ab27c73600e7708e8dfebc9a40d1f
Serhii Balaba <balabaserhii@gmail.com>
2024-09-24 21:30:36 +0200

    add api, slice

---
+++ src/store/campersSlice/campersSlice.js
@@ -0,0 +1,17 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+
+export const fetchCampers = createAsyncThunk(
+  "campers/fetchCampers",
+  async () => {
+    const response = await axios.get(
+      "https://66b1f8e71ca8ad33d4f5f63e.mockapi.io/campers"
+    );
+    return response.data;
+  }
+);
+
+// const campersSlice = createSlice({
+//     name: 'campers',
+
+// })
